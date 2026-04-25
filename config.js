830f407bdd3514473695ead7f67e16b83336a2e6
lasloandriessen-hue <lasloandriessen@gmail.com>
2026-04-25 23:29:43 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://yqiwehmpddqscilmrxhr.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxaXdlaG1wZGRxc2NpbG1yeGhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNDQzNzEsImV4cCI6MjA4ODgyMDM3MX0.r1fjqd4mtsE3kb4ESBY0STjWSfi4w3dAH4ydspkVmEU',
+};
